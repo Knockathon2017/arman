9787d987b4714575007ec852d0f953f1610585a5
Mohit Verma <mohitverma3222@gmail.com>
2016-08-06 16:06:24 +0530

    fsdfsdf

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ var app = express();
 mongoose.connect('mongodb://localhost:27017/test', function(err, database) {
     if (err) {
         console.log(err);
-        process.exit(1);
     }
 
     // Save database object from the callback for reuse.
