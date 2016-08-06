1c359e1f2146f578443881c60510a82a68868a08
Rahul Kumar <rahul.k@exzeo.com>
2016-08-06 08:14:33 +0530

    user update and create end point

--- controller/User.js
+++ controller/User.js
@@ -4,6 +4,7 @@ var UserDetail = mongoose.model('User', require('../model/User'));
 
 
 exports.list = function(req, res, next) {
+	console.log(req);
     UserDetail.find().exec(function(err, subevnts) {
         if (err) {
             return next(err);
@@ -23,4 +24,47 @@ exports.create = function(req, res, next) {
             res.json(saveop);
         }
     });
+};
+
+exports.read = function(req, res) {	
+    res.json(res);
+};
+
+exports.update = function(req, res, next) {
+
+	var response = {};
+	
+	UserDetail.findOne({username: req.params.username}).exec(function(err, user){
+		if(err || user == null) {
+			response = {"error" :404, "message": "user not found."}
+			res.json(response)
+		} else {
+
+			console.log(req.body)
+			console.log(user)
+			if(req.body.lastname !== undefined){
+
+				user.lastname = req.body.lastname;
+			}
+			if(req.body.email !== undefined) {
+				user.email = req.body.email;
+			}
+			if(req.body.coordinates !== undefined) {
+				user.coordinates = req.body.coordinates;
+			}
+
+			user.save(function(err){
+				if(err) {
+					response = {"error": 400, "message": err}
+				} else {
+					response = user;
+				}
+				res.json(response)
+				
+			});
+		}
+	
+		
+		
+	});
 };
